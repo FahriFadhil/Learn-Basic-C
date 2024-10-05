ffd64862a9adde0c74bcb7134b2a11c3c6431f20
Fahri Fadhil Muhammad <fahrifadhil.m22@gmail.com>
2024-10-05 21:26:36 +0700

    Repetition; i, c

--- SOCS's Directory/Repetition/e.c
+++ SOCS's Directory/Repetition/e.c
@@ -2,7 +2,13 @@
 
 int main()
 {
-    // Skip
     int t, x, p;
+    scanf("%d", &t);
+    for (int i = 0; i < 3; i++)
+    {
+        scanf("%d %d", &x, &p);
+        printf("%d\n", (x >> p) & 1);
+    }
+
     return 0;
 }
--- SOCS's Directory/Repetition/i.c
+++ SOCS's Directory/Repetition/i.c
@@ -2,6 +2,21 @@
 
 int main()
 {
+    int n, j, l, b, sum;
+    scanf("%d\n%d %d %d", &n, &j, &l, &b);
 
+    int nSum[n];
+    sum += j + l + b;
+
+    for (int i = 0; i < n; i++)
+    {
+        scanf("%d", &nSum[i]);
+        sum += nSum[i];
+    }
+
+    double av = (double)sum / (3 + n);
+    printf("%s\n", j >= av ? "Jojo lolos" : "Jojo tidak lolos");
+    printf("%s\n", l >= av ? "Lili lolos" : "Lili tidak lolos");
+    printf("%s\n", b >= av ? "Bibi lolos" : "Bibi tidak lolos");
     return 0;
 }
