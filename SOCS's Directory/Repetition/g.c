a4650b669984e200d40c6f995654fefa8597c47d
Fahri Fadhil Muhammad <fahrifadhil.m22@gmail.com>
2024-10-05 13:15:09 +0700

    Repetition; G, H

--- SOCS's Directory/Repetition/g.c
+++ SOCS's Directory/Repetition/g.c
@@ -2,6 +2,19 @@
 
 int main()
 {
+    int n;
+    scanf("%d", &n);
+
+    int anim[n];
+    int sum = 0;
+
+    for (int i = 0; i < n; i++)
+    {
+        scanf("%d", &anim[i]);
+        sum += anim[i];
+    }
+
+    printf("%d\n", sum);
 
     return 0;
 }
--- SOCS's Directory/Repetition/h.c
+++ SOCS's Directory/Repetition/h.c
@@ -2,6 +2,15 @@
 
 int main()
 {
-
+    int n;
+    scanf("%d", &n);
+    for (int i = 0; i < n; i++)
+    {
+        for (int j = 0; j < n; j++)
+        {
+            printf("*");
+        }
+        printf("\n");
+    }
     return 0;
 }
