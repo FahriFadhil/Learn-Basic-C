fbb9d82b728744d6b962d4677a99d7c7e8738d54
Fahri Fadhil Muhammad <fahrifadhil.m22@gmail.com>
2024-10-11 15:59:00 +0700

    .

--- SOCS's Directory/Array & Pointer/l.c
+++ SOCS's Directory/Array & Pointer/l.c
@@ -2,6 +2,26 @@
 
 int main()
 {
+    int t;
+    scanf("%d", &t);
+    int arrArrange[t], arrRes[t];
+    for (int i = 0; i < t; i++)
+    {
+        scanf("%d", &arrArrange[i]);
+    }
+    for (int i = 0; i < t; i++)
+    {
+        scanf("%d", &arrRes[arrArrange[i]]);
+    }
+    for (int i = 0; i < t; i++)
+    {
+        printf("%d", arrRes[i]);
+        if (t != (i + 1))
+        {
+            printf(" ");
+        }
+    }
+    printf("\n");
 
     return 0;
 }
