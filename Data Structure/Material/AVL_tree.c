3ef972085f333d32cc601bbf76d73cd8779ec072
Fahri Fadhil Muhammad <fahrifadhil.m22@gmail.com>
2025-06-02 23:02:00 +0700

    update

--- Data Structure/Material/AVL_tree.c
+++ Data Structure/Material/AVL_tree.c
@@ -98,43 +98,49 @@ Node* deleteNode(Node* root, int val) {
     else if (val > root->val)
         root->right = deleteNode(root->right, val);
     else {
-        if ((root->left == NULL) || (root->right == NULL)) {
-            Node *temp = root->left ? root->left : root->right;
-            if (temp == NULL) {
-                temp = root;
-                root = NULL;
-            } else {
-                *root = *temp;
-            }
-            free(temp);
-        } else {
-            Node* temp = minValueNode(root->right);
-            root->val = temp->val;
-            root->right = deleteNode(root->right, temp->val);
+        if (!root->left && !root->right) {
+            free(root);
+            return NULL;
         }
-    }
+        if (!root->left) {
+            Node* temp = root->right;
+            free(root);
+            return temp;
+        }
+        if (!root->right) {
+            Node* temp = root->left;
+            free(root);
+            return temp;
+        }
 
-    if (root == NULL)
-        return root;
+        Node* temp = minValueNode(root->right);
+        root->val = temp->val;
+        root->right = deleteNode(root->right, temp->val);
+    }
 
     root->height = 1 + max(height(root->left), height(root->right));
     int balance = getBalance(root);
 
-    if (balance > 1 && getBalance(root->left) >= 0)
-        return rightRotate(root);
-    if (balance > 1 && getBalance(root->left) < 0) {
-        root->left = leftRotate(root->left);
-        return rightRotate(root);
+    if (balance > 1) {
+        if (getBalance(root->left) >= 0) {
+            return rightRotate(root);  // LL Case
+        } else {
+            root->left = leftRotate(root->left);  // LR Case
+            return rightRotate(root);
+        }
     }
-    if (balance < -1 && getBalance(root->right) <= 0)
-        return leftRotate(root);
-    if (balance < -1 && getBalance(root->right) > 0) {
-        root->right = rightRotate(root->right);
-        return leftRotate(root);
+    if (balance < -1) {
+        if (getBalance(root->right) <= 0) {
+            return leftRotate(root);  // RR Case
+        } else {
+            root->right = rightRotate(root->right);  // RL Case
+            return leftRotate(root);
+        }
     }
 
     return root;
 }
+
 
 void inorder(Node *root) {
     if (root) {
